a8ed0202764d1a54ac14163a086b07e67ad3d22c
David Sofield <davidsofield@gmail.com>
2019-09-22 10:12:06 -0400

    Updates

--- src/components/FeaturesBlock.js
+++ src/components/FeaturesBlock.js
@@ -9,19 +9,19 @@ export default class FeaturesBlock extends React.Component {
         return (
             <section id={_.get(this.props, 'section.section_id')} className={'block features-block bg-' + _.get(this.props, 'section.bg') + ' outer'}>
               <div className="block-header inner-small">
-                <h2 className="block-title">{_.get(this.props, 'section.title')}</h2>
-                {_.get(this.props, 'section.subtitle') && 
+                <h1 className="block-title">{_.get(this.props, 'section.title')}</h1>
+                {_.get(this.props, 'section.subtitle') &&
                 <p className="block-subtitle">
                   {htmlToReact(_.get(this.props, 'section.subtitle'))}
                 </p>
                 }
               </div>
-              {_.get(this.props, 'section.featureslist') && 
+              {_.get(this.props, 'section.featureslist') &&
               <div className="inner">
                 {_.map(_.get(this.props, 'section.featureslist'), (feature, feature_idx) => (
                 <div key={feature_idx} className="block-item">
                   <div className="grid">
-                    {_.get(feature, 'image') && 
+                    {_.get(feature, 'image') &&
                     <div className="cell block-preview">
                       <img src={safePrefix(_.get(feature, 'image'))} alt={_.get(feature, 'title')} />
                     </div>
@@ -31,7 +31,7 @@ export default class FeaturesBlock extends React.Component {
                       <div className="block-copy">
                         {markdownify(_.get(feature, 'content'))}
                       </div>
-                      {_.get(feature, 'actions') && 
+                      {_.get(feature, 'actions') &&
                         <CtaButtons {...this.props} actions={_.get(feature, 'actions')} />
                       }
                     </div>
